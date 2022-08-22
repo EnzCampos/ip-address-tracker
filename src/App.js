0777fa863f79926d0d2f97165a8cb17799902f44
Enzo Campos <enzocampos@gmail.com>
2022-08-22 19:32:10 -0300

    Added info-box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import React from 'react'
+
 function App() {
+  const [info, setInfo] = React.useState({
+    "ipAddress": "192.212.174.101",
+    "location": "Brooklin, NY 10001",
+    "timezone": "UTC-05:00",
+    "isp": "Starlink"
+  })
+
   return (
     <div className="App">
       <div className="header">
@@ -8,7 +17,20 @@ function App() {
           <button className="submit-button">{'>'}</button>
         </div>
       </div>
-      <div>Info PlaceHolder</div>
+      <div className="info-box">
+        <div className='ip info-item-border'>IP ADDRESS
+          <h3>{info.ipAddress}</h3>
+        </div>
+        <div className='location info-item-border'>LOCATION
+          <h3>{info.location}</h3>
+        </div>
+        <div className='timezone info-item-border'>TIMEZONE
+          <h3>{info.timezone}</h3>
+        </div>
+        <div className='isp info-item'>ISP
+          <h3>{info.isp}</h3>
+        </div>
+      </div>
       <div>Map PlaceHolder</div>
     </div>
   );
