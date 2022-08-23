6dc6f9c78e6cbfdcc6100f3cdf9155909786af5e
Enzo Campos <enzocampos@gmail.com>
2022-08-23 20:27:26 -0300

    Added input handler

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 function App() {
   const [info, setInfo] = React.useState({
-    "ipAddress": "192.212.174.101",
+    "ipAddress": "",
     "location": "Brooklin, NY 10001",
     "timezone": "UTC-05:00",
     "isp": "Starlink",
     "position": [40.661311, -73.941910]
   })
 
-  
+  function handleChange(event) {
+    setInfo(prevValue => ({
+      ...prevValue,
+      [event.target.name]: event.target.value,
+    }))
+  }
 
   function getPosition() {
     fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_Ace0JgifMIIn5yoWn3ux4cW2sAfyh&ipAddress=${info.ipAddress}`)
@@ -18,7 +23,7 @@ function App() {
         .then(res => setInfo({
             "ipAddress": res.ip,
             "location": `${res.location.region}, ${res.location.city}`,
-            "timezone": res.location.timezone,
+            "timezone": `UTC ${res.location.timezone}`,
             "isp": res.isp,
             "position": [res.location.lat, res.location.lng]
         }))
@@ -29,13 +34,13 @@ function App() {
       <div className="header">
         <h1 className="title">Ip Address Tracker</h1>
         <div className="input-box">
-          <input type="text" name="address" placeholder="Search for any IP address or domain" className="address-input"/>
+          <input type="text" name="ipAddress" placeholder="Search for any IP address or domain" className="address-input" onChange={handleChange}/>
           <button className="submit-button" onClick={getPosition}>{'>'}</button>
         </div>
       </div>
       <div className="info-box">
         <div className='ip info-item-border'>IP ADDRESS
-          <h3>{info.ipAddress}</h3>
+          <h3>{info.ipAddress ? info.ipAddress : "192.212.174.101"}</h3>
         </div>
         <div className='location info-item-border'>LOCATION
           <h3>{info.location}</h3>
@@ -55,7 +60,7 @@ function App() {
           />
           <Marker position={info.position}>
             <Popup>
-              This is the location you've put.
+              This is the approximated location you've put.
             </Popup>
           </Marker>
         </MapContainer>
