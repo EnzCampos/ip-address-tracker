da4648b488d2035fc5fafd8f2d505e7a45bd644f
Enzo Campos <enzocampos@gmail.com>
2022-08-23 20:49:27 -0300

    Added input Regex for conditional API fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   const [info, setInfo] = React.useState({
-    "ipAddress": "",
+    "ipAddress": "192.212.174.101",
     "location": "Brooklin, NY 10001",
     "timezone": "UTC-05:00",
     "isp": "Starlink",
     "position": [40.661311, -73.941910]
   })
+  const [input, setInput] = React.useState('')
 
   function handleChange(event) {
-    setInfo(prevValue => ({
-      ...prevValue,
-      [event.target.name]: event.target.value,
-    }))
+    setInput(event.target.value,)
   }
 
   function getPosition() {
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_Ace0JgifMIIn5yoWn3ux4cW2sAfyh&ipAddress=${info.ipAddress}`)
+    let inputHandler = '';
+    //HTTP or HTTPS Regex test
+    if (/^https?:\/\//.test(input)) {
+      inputHandler = `domain=${input}`;
+    }
+    //IPV4 Regex Test
+    if (/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(input)) {
+      inputHandler = `ipAddress=${input}`;
+    }
+    //IPV6 Regex Test
+    if (/((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/.test(input)) {
+      inputHandler = `ipAddress=${input}`;
+    }
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_Ace0JgifMIIn5yoWn3ux4cW2sAfyh&${inputHandler}`)
         .then(res => res.json())
         .then(res => setInfo({
             "ipAddress": res.ip,
@@ -37,6 +48,7 @@ function App() {
           <input type="text" name="ipAddress" placeholder="Search for any IP address or domain" className="address-input" onChange={handleChange}/>
           <button className="submit-button" onClick={getPosition}>{'>'}</button>
         </div>
+        <p className='disclaimer'>If you put an invalid IP or domain it'll use your current device IP. The app won't save any of your information.</p>
       </div>
       <div className="info-box">
         <div className='ip info-item-border'>IP ADDRESS
